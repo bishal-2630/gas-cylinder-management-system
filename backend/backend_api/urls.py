847e1f000b322550e03e6c7a260e70f8da2759d6
Bishal <kbishal177@gmail.com>
2026-03-20 21:34:46 +0545

    Fix imports in urls.py and restore API functionality

--- backend/backend_api/urls.py
+++ backend/backend_api/urls.py
@@ -14,7 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import RedirectView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/api/')),
